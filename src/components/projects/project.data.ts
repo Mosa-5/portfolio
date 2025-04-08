95ee45a8e59ade09070c9ad1c017a7c68abcbef6
Mosacd <levanmosiashvili5@gmail.com>
2025-04-09 01:35:34 +0400

    images fixed by using imports instead of public

--- src/components/projects/project.data.ts
+++ src/components/projects/project.data.ts
@@ -1,8 +1,10 @@
+import gymgear from "@/assets/projectImages/GymGear.png";
+import koko from "@/assets/projectImages/KoKo.png";
 
 export const projectData: ProjectDataType[] = [
     {
         title: "GymGear",
-        image: "/public/images/gymgear.png", 
+        image: gymgear, 
         description: "E-commerce platform, selling gym equipment,profiles page storing past order info as well as wishlisted items and personal info, ability to leave reviews for products",
         techstack: "React, Tailwind, supabase for database and backend abstraction",
         webLink: "https://gym-app-4jrt.vercel.app",
@@ -10,7 +12,7 @@ export const projectData: ProjectDataType[] = [
     },
     {
         title: "KoKo",
-        image: "/public/images/koko.png", 
+        image: koko, 
         description: "fun little website, where users can learn/practice sign language",
         techstack: "React, Tailwind, supabase for database and backend abstraction",
         webLink: "https://gym-app-4jrt.vercel.app",
