a09c5bbd2745392207819868c6361547a8914824
Levan Mosiashvili <levanmosiashvili5@gmail.com>
2026-03-29 18:02:31 +0400

    intial load time optimized

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useRef, useState } from 'react'
-import About from './components/about'
+import { lazy, Suspense, useEffect, useRef, useState } from 'react'
 import Header from './components/header'
 import Hero from './components/hero'
-import Projects from './components/projects'
-import TechStack from './components/techstack'
-import RobotChatbot from './components/chatBot'
+
+const TechStack = lazy(() => import('./components/techstack'))
+const Projects = lazy(() => import('./components/projects'))
+const About = lazy(() => import('./components/about'))
+const RobotChatbot = lazy(() => import('./components/chatBot'))
 
 
 function App() {
@@ -25,17 +26,16 @@ function App() {
 
   useEffect(() => {
     Promise.all([
-      document.fonts.load('300 1em Poppins'),
-      document.fonts.load('400 1em Poppins'),
+      document.fonts.load('500 1em Poppins'),
       document.fonts.load('600 1em Poppins'),
-      document.fonts.load('700 1em Poppins'),
+      document.fonts.load('800 1em Poppins'),
     ]).then(() => {
       setFontLoaded(true);
     });
   }, []);
 
   if (!fontLoaded) {
-    return <div  className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-20 h-20 border-8 border-[#03055e5e] border-t-[#03045E] rounded-full animate-spin"
+    return <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-20 h-20 border-8 border-[#d8a01340] border-t-[#d8a013] rounded-full animate-spin"
     ></div>;
   }
 
@@ -43,12 +43,14 @@ function App() {
     <div className='w-full relative sm:px-5 py-10 lg:p-10 max-w-7xl m-auto text-center'>
       <Header scrollTo = {scrollTo} />
       <Hero/>
+      <Suspense>
       <div className='flex flex-col gap-40 mt-40 fancy-block *:pt-10'>
       <TechStack homeRef = {sectionRefs.home}/>
       <Projects projectsRef = {sectionRefs.projects} />
       <About aboutRef = {sectionRefs.about}/>
       </div>
       <RobotChatbot/>
+      </Suspense>
     </div>
   )
 }
